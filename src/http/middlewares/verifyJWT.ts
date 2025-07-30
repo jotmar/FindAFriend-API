1be4e3772be912e56212c2495f0024b81583e534
João Marcos <joaomarcos.vidcar@gmail.com>
2025-07-29 21:17:39 -0300

    fix(middleware): await request at verifyJWT

--- src/http/middlewares/verifyJWT.ts
+++ src/http/middlewares/verifyJWT.ts
@@ -2,7 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function verifyJWT(request: FastifyRequest, reply: FastifyReply) {
 	try {
-		request.jwtVerify()
+		await request.jwtVerify()
 	} catch (error) {
 		return reply.status(401).send({ message: 'Unauthorized' })
 	}
